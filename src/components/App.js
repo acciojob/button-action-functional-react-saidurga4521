d7f44060351afb4287a5d89c49e656d5a6fda515
Durga Prasad <saikamireddi3@gmail.com>
2025-05-02 12:10:58 +0530

    displaying is done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [value, setvalue] = useState("");
+  const display = () => {
+    setvalue(
+      "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy"
+    );
+  };
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={display}>
+        Click
+      </button>
+      <p id="para">{value}</p>
     </div>
   );
 }
-
 
 export default App;
